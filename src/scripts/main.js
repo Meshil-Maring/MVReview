09dd9f5044e6172f7871f2828b26532dc60973c2
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-03-26 19:02:22 +0530

    Adding Footer

---
+++ src/scripts/main.js
@@ -0,0 +1,17 @@
+function loadComponent(id, file) {
+  fetch(file)
+    .then((response) => response.text())
+    .then((data) => (document.getElementById(id).innerHTML = data))
+    .catch((error) => console.error(error));
+}
+
+loadComponent("header", "../components/common/header.html");
+
+// Home page components
+loadComponent("hero-section", "../components/home/hero-section.html");
+loadComponent("now-in-theaters", "../components/home/now-in-theaters.html");
+loadComponent(
+  "explore-whats-streaming",
+  "../components/home/explore-whats-streaming.html"
+);
+loadComponent("footer", "../components/common/footer.html");
